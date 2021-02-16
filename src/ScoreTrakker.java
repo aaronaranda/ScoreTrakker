aa14cf457ac26ac3cbd3bc5e5cd6a452ac4c2cf4
Aaron Aranda <aaronjaranda@mymail.mines.edu>
2021-02-16 01:22:56 -0700

    init

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,4 +1,48 @@
+import java.util.*;
+import java.io.*;
+import javax.swing.JFileChooser;
+
 
 public class ScoreTrakker {
-
+	private ArrayList<Student> students;
+	
+	private void loadDataFromFile() {
+		FileReader reader = null;
+		String fileName = null;
+		JFileChooser fileChooser = new JFileChooser();
+		if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			File selected = fileChooser.getSelectedFile();
+			fileName = selected.getAbsolutePath();
+		}
+		try {
+			reader = new FileReader(fileName);
+		} catch (FileNotFoundException e) {
+			System.out.println("File not Found");
+		}
+		Scanner in = new Scanner(reader);
+		
+		while (in.hasNext()){
+			String line = in.next();	
+			String[] temp = line.split(" ");
+			String name = temp[0] + " " + temp[1];
+			int score = Interger.valueOf(temp[2]);
+			Student student = new Student(name, score)
+			}
+		
+	}
+	
+	private void printInOrder() {
+		
+	}
+	
+	private void processFiles() {
+		
+	}
+	
+	public static void main(String[] args) {
+		ScoreTrakker trakker = new ScoreTrakker();
+		trakker.loadDataFromFile();
+	}
+	
+	
 }
--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,24 @@
 
-public class Student {
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
 	}
 
+	
+	public int compareTo(Student other) {
+		return this.score > other.score ? 1 : this.score < other.score ? -1 : 0;
+	}
+	
+	@Override
+	public String toString() {
+		//Not sure if this will work
+		return (this.name + " " + this.score); 
+	}
+	
+	
 }
