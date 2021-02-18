06405201e4a088ff0de45e01b49ec71a31e9b055
aaronaranda <aaronjaranda@mymail.mines.edu>
2021-02-17 18:30:49 -0700

    Final changes

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,65 +1,53 @@
+//Authors: Aaron Aranda and Lauren Loe
 import java.util.*;
 import java.io.*;
-import javax.swing.JFileChooser;
-
+import javax.swing.JFileChooser;    //Used only in part 1
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
 	private String[] files = {"scores.txt", "badscores.txt", "nofile.txt" };
 
-	private void loadDataFromFile() {
-        students = new ArrayList<Student>(10);
-
-        //File Handling
-        for (String f: files) {
-           FileReader reader = null;
-           Scanner in = null;
-           try {
-	           reader = new FileReader(f);
-		       in = new Scanner(reader);
-		   } catch (FileNotFoundException e) {
-	           System.out.println("Can't open file: " + f);
-               continue;
-           }
-            
-		
-            //Data Collection
-       
-	    	while (in.hasNext()) {
-                Student student;
-        	    String name = in.nextLine();	
-                String stringScore = in.nextLine();
-                int score = 0;
-                try {
-                    score = Integer.parseInt(stringScore);
-                } catch (NumberFormatException n) {
-                    System.out.println("Incorrect format for " + name + 
-                            " not at a valid score: " + stringScore);
-                }      		
-    		student = new Student(name, score);       
-            students.add(student);
-	    }
+    private void loadDataFromFile(String file) throws FileNotFoundException {
+        students = new ArrayList<Student>(10);		
+        FileReader reader = new FileReader(file);
+        Scanner in = new Scanner(reader);
+    	while (in.hasNext()) {
+            Student student = null;
+       	    String name = in.nextLine();	
+            String stringScore = in.nextLine();
+            int score = 0;
+            try {
+                score = Integer.parseInt(stringScore);
+                student = new Student(name, score);
+                students.add(student);
+            } catch (NumberFormatException n) {
+                System.out.println("Incorrect format for " + name + 
+                        " not at a valid score: " + stringScore);
+            }      		
+        }
     }
-}
-	
+
 	private void printInOrder() {
-        for (Student s: this.students) {
+        for (Student s : students) {
             if (s != null) {
             System.out.println(s.toString());
             } 
     	}
     }
-
 	
-	private void processFiles() {
-		
+	public void processFiles() {
+        for (String f: files) {
+            try {
+                loadDataFromFile(f);
+                printInOrder();
+            } catch (FileNotFoundException e) {
+                System.out.println("Can't open file: " + f);
+            }       
+        }
 	}
 	
 	public static void main(String[] args) {
 		ScoreTrakker trakker = new ScoreTrakker();
-		trakker.loadDataFromFile();
-        trakker.printInOrder();
+		trakker.processFiles();
 	}
-	
-	
 }
